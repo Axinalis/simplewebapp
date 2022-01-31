82a8f9c392f976e633976f813bb6f688d43a786e
Axinalis <antontrus@gmail.com>
2022-01-31 15:10:54 +0300

    Added liquibase

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO employee (first_name, second_name, department_id, job_title, gender, date_of_birth)
-VALUES('Anton', 'Trus', 5, 'Junior Developer', 'MALE', '2000-05-29');
-INSERT INTO employee (first_name, second_name, department_id, job_title, gender, date_of_birth)
-VALUES('Nikolay', 'Golubov', 3, 'Project manager', 'MALE', '1998-08-20');
-INSERT INTO employee (first_name, second_name, department_id, job_title, gender, date_of_birth)
-VALUES('Andrey', 'Shulgach', 3, 'Team lead', 'MALE', '1985-01-02');
-INSERT INTO employee (first_name, second_name, department_id, job_title, gender, date_of_birth)
-VALUES('Natalia', 'Mironova', 4, 'HR', 'FEMALE', '1995-04-15');
